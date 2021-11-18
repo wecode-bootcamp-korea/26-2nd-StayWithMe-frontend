172bf76e66d2681e8c346c9bda6474cd5c118718
jyb8509 <bestdung8509@gmail.com>
2021-11-18 20:11:02 +0900

    수정

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = 'http://52.79.212.98:8000';
-export const KAOKAO_LOGIN_URL = '52.79.212.98:8000';
-export const ACCESS_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_JavaScript_KEY}&redirect_uri=http://localhost:3000/AuthCallbacKakao/kakao&response_type=code`;
