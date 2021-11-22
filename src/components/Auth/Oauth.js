81920d6d3ef3adc9515e15afab3c6a4dae0fc55f
jyb8509 <bestdung8509@gmail.com>
2021-11-22 16:53:52 +0900

    modify: 소셜로그인 최종

--- src/components/Auth/Oauth.js
+++ src/components/Auth/Oauth.js
@@ -2,4 +2,4 @@
 
 export const REDIRECT_URI = 'http://localhost:3000/users/signin';
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=c20e78f6f53398ae9e12a866b8790ce1&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=00d03738b207f333d61f57b9d9937fcf&redirect_uri=${REDIRECT_URI}&response_type=code`;
--- src/components/Auth/RedirectHandler.js
+++ src/components/Auth/RedirectHandler.js
@@ -5,10 +5,11 @@ const Redirect = () => {
   let accessCode = new URL(window.location.href).searchParams.get('code');
   // let accessToken;
 
+  console.log(accessCode);
+
   const bodyData = {
     grant_type: 'authorization_code',
-    // client_id: '00d03738b207f333d61f57b9d9937fcf',
-    client_id: 'c20e78f6f53398ae9e12a866b8790ce1',
+    client_id: '00d03738b207f333d61f57b9d9937fcf',
     redirect_uri: REDIRECT_URI,
     code: accessCode,
   };
@@ -24,21 +25,23 @@ const Redirect = () => {
         'Content-Type': 'application/x-www-form-urlencoded;',
         Accept: 'application/json',
       },
+      // body: formData,
       body: queryStringBody,
     })
       .then(res => res.json())
       .then(result => {
+        console.log('result', result);
         let accessToken = result.access_token;
 
         accessToken &&
-          fetch('http://10.58.2.138:8000/users/signin', {
-            // method: 'POST',
+          fetch('http://10.58.4.242:8000/users/signin', {
+            method: 'POST',
             headers: {
               Authorization: accessToken,
             },
           })
             .then(res => res.json())
-            .then(result => localStorage.setItem('yes', result.access_token));
+            .then(result => localStorage.setItem('token', accessToken));
       });
   }, []);
 
