e5eb35db65e1b3ab99ed2ed47eff9a3c5d13ec35
newjinny <syj9437@gmail.com>
2021-11-25 21:54:57 +0900

    wip

--- src/pages/DetailPage/ShowDateBox.js
+++ src/pages/DetailPage/ShowDateBox.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ShowDateBox = () => {
-  return <div>ShowDateBox 입니다.</div>;
+const ShowDateBox = ({ check_in, check_out }) => {
+  return <ShowDate>{`${check_in}-${check_out}`}</ShowDate>;
 };
 
+const ShowDate = styled.div``;
 export default ShowDateBox;
